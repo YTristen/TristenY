c1c21a7e2cc7533692fae80bb708ed121d78dc81
Tristen_Y <425247746@qq.com>
2024-07-28 01:14:02 +0800

    base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/TristenY/",
   integrations: [react(), tailwind()],
   site: "https://tristen.icu",
 
